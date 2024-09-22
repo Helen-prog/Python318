7a1de95e9b974a741d3f54aae3621585d577898a
Helen <elena.kozovyakina@gmail.com>
2024-09-22 16:27:50 +0300

    practice

--- fifth/programming_blog/blog/utils.py
+++ fifth/programming_blog/blog/utils.py
@@ -1,17 +1,23 @@
 from .models import *
+from django.db.models import Count
 
 menu = [
     {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Войти', 'url_name': 'index'},
 ]
 
 
 class DataMixin:
+    paginate_by = 3
+
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.all()
+        cats = Category.objects.annotate(Count('blog'))
 
-        context['menu'] = menu
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(0)
+
+        context['menu'] = user_menu
         context['cats'] = cats
 
         if 'cat_selected' not in context:
--- fifth/programming_blog/blog/views.py
+++ fifth/programming_blog/blog/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from .models import *
 from .forms import *
 from django.urls import reverse_lazy
 from .utils import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 
 
 class BlogHome(DataMixin, ListView):
+    # paginate_by = 3
     model = Blog  # posts = Blog.objects.all()
     template_name = "blog/index.html"
     context_object_name = "posts"
@@ -28,13 +33,12 @@ class ShowPost(DataMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['title'] = context['post']
-        # context['menu'] = menu
         c_def = self.get_user_context(title=context['post'])
         return dict(list(context.items()) + list(c_def.items()))
 
 
 class BlogCategory(DataMixin, ListView):
+    # paginate_by = 3
     model = Blog
     template_name = "blog/index.html"
     context_object_name = "posts"
@@ -44,23 +48,44 @@ class BlogCategory(DataMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['title'] = "Категория - " + str(context['posts'][0].cat)
-        # context['cat_selected'] = context['posts'][0].cat_id
-        # context['menu'] = menu
         c = Category.objects.get(slug=self.kwargs['cat_slug'])
         c_def = self.get_user_context(title="Категория - " + str(c.name), cat_selected=c.pk)
         return dict(list(context.items()) + list(c_def.items()))
 
 
-class AddPage(DataMixin, CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'blog/addpage.html'
     success_url = reverse_lazy('index')
+    login_url = reverse_lazy('login')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['title'] = "Добавление статьи"
-        # context['menu'] = menu
         c_def = self.get_user_context(title="Добавление статьи")
         return dict(list(context.items()) + list(c_def.items()))
 
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'blog/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
